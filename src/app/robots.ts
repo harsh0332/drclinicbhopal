c1c4928f72940c58d5afe88390edc8a5573591ea
harsh0332 <aditichouksey116@gmail.com>
2026-07-19 02:45:53 +0530

    seo: update metadata and config for src/app/robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,10 +2,16 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: "https://www.babystepsclinic.in/sitemap.xml",
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: ["GPTBot", "PerplexityBot", "ClaudeBot", "Google-Extended", "CCBot"],
+        allow: "/",
+      }
+    ],
+    sitemap: "https://babystepsnewbornclinic.com/sitemap.xml",
   };
 }
